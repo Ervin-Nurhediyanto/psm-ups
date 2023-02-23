b7403e05ffaab865a9bedd4e25ccb0831caa5bc3
Ervin Nurhediyanto <cafeinai311@gmail.com>
2023-02-23 10:48:06 +0700

    Big-M-Value

--- src/components/Content/Process/Big-M/Tables/Value/T-V-Key-Row.vue
+++ src/components/Content/Process/Big-M/Tables/Value/T-V-Key-Row.vue
@@ -1,21 +1,21 @@
 <template>
   <span>
     <span v-if="n === 0">
-      <span>
-        {{ Math.round(((data.table_1st[iR - 1][iC - 1][0]) + Number.EPSILON) * 100) / 100 }}
-      </span>
       <span v-if="Math.round(((data.table_1st[iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 !== 0">
-        <span v-if="data.table_1st[iR - 1][iC - 1][1] > 0">+</span>
         {{ Math.round(((data.table_1st[iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 }}M
+      </span>
+      <span v-if="(Math.round(((data.table_1st[iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 !== 0 && Math.round(((data.table_1st[iR - 1][iC - 1][0]) + Number.EPSILON) * 100) / 100 !== 0) || (Math.round(((data.table_1st[iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 === 0)">
+        <span v-if="data.table_1st[iR - 1][iC - 1][0] > 0 && Math.round(((data.table_1st[iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 !== 0">+</span>
+        {{ Math.round(((data.table_1st[iR - 1][iC - 1][0]) + Number.EPSILON) * 100) / 100 }}
       </span>
     </span>
     <span v-else>
-      <span>
-        {{ Math.round(((data.table_it[n - 1][iR - 1][iC - 1][0]) + Number.EPSILON) * 100) / 100 }}
-      </span>
       <span v-if="Math.round(((data.table_it[n - 1][iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 !== 0">
-        <span v-if="data.table_it[n - 1][iR - 1][iC - 1][1] > 0">+</span>
         {{ Math.round(((data.table_it[n - 1][iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 }}M
+      </span>
+      <span v-if="(Math.round(((data.table_it[n - 1][iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 !== 0 && Math.round(((data.table_it[n - 1][iR - 1][iC - 1][0]) + Number.EPSILON) * 100) / 100 !== 0) || Math.round(((data.table_it[n - 1][iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 === 0">
+        <span v-if="data.table_it[n - 1][iR - 1][iC - 1][0] > 0 && Math.round(((data.table_it[n - 1][iR - 1][iC - 1][1]) + Number.EPSILON) * 100) / 100 !== 0">+</span>
+        {{ Math.round(((data.table_it[n - 1][iR - 1][iC - 1][0]) + Number.EPSILON) * 100) / 100 }}
       </span>
     </span>
   </span>
